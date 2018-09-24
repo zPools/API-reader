dfe938d600336c86c5d10493f61b564636066d20
Mike <zpoolsde@gmail.com>
2018-09-24 10:29:39 +0200

    kucoin added
    Signed-off-by: Mike <zpoolsde@gmail.com>

--- core/clean-db.php
+++ core/clean-db.php
@@ -11,6 +11,7 @@ if ($conn->connect_error) {
 } 
 // Delete data from database "currently we only need btc price"	
 // TODO: Make it with less commands
+// TODO2: If more exchanges come, the naming of the JSON can be a issue. Make it that only needed sql cleans per exchange are made
 $sqldel1 = "DELETE FROM $exchange WHERE `coin` REGEXP '^ETH_'";
 $sqldel2 = "DELETE FROM $exchange WHERE `coin` REGEXP '^USD_'";
 $sqldel3 = "DELETE FROM $exchange WHERE `coin` REGEXP '^CNY_'";
@@ -20,115 +21,121 @@ $sqldel6 = "DELETE FROM $exchange WHERE `coin` REGEXP '^RUB_'";
 $sqldel7 = "UPDATE $exchange SET `coin` = SUBSTRING(coin,5) WHERE `coin` REGEXP '^BTC_'";
 $sqldel8 = "DELETE FROM $exchange WHERE `coin` REGEXP '^USDT_'";
 $sqldel9 = "DELETE FROM $exchange WHERE `coin` REGEXP '^XMR_'";
-$sqldel10 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/USNBT'";
-$sqldel11 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/USD'";
-$sqldel12 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/BCH'";
-$sqldel13 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/CNNBT'";
-$sqldel14 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/DASH'";
-$sqldel15 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/LTC'";
-$sqldel16 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/TUSD'";
-$sqldel17 = "UPDATE $exchange SET `coin` = CONCAT(LEFT(`coin`, CHAR_LENGTH(`coin`) -4), '') WHERE `coin` REGEXP '\w*/BTC'";
-
+$sqldel10 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/USNBT' OR `coin` REGEXP '\w*-USNBT'";
+$sqldel11 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/USD' OR `coin` REGEXP '\w*-USD'";
+$sqldel12 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/BCH' OR `coin` REGEXP '\w*-BCH'";
+$sqldel13 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/CNNBT' OR `coin` REGEXP '\w*-CNNBT'";
+$sqldel14 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/DASH' OR `coin` REGEXP '\w*-DASH'";
+$sqldel15 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/LTC' OR `coin` REGEXP '\w*-LTC'";
+$sqldel16 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*/TUSD' OR `coin` REGEXP '\w*-TUSD' OR `coin` REGEXP '\w*-USDT'";
+$sqldel18 = "DELETE FROM $exchange WHERE `coin` REGEXP '\w*-NEO' OR `coin` REGEXP '\w*-ETH' OR `coin` REGEXP '\w*-KCS'";
+$sqldel17 = "UPDATE $exchange SET `coin` = CONCAT(LEFT(`coin`, CHAR_LENGTH(`coin`) -4), '') WHERE `coin` REGEXP '\w*/BTC' OR `coin` REGEXP '\w*-BTC'";
 
 
 if ($conn->query($sqldel1) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND ETH from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED ETH from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel1 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel2) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND USD from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED USD from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel2 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel3) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND CNY from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED CNY from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel3 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel4) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND EUR from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED EUR from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel4 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel5) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND JPY from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED JPY from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel5 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel6) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND RUB from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED RUB from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel6 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel8) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND USDT from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED USDT from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}	
+	{echo "Error: " . $sqldel8 . "<br>" . $conn->error;}	
 
 
 if ($conn->query($sqldel9) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND XMR from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED XMR from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}	
+	{echo "Error: " . $sqldel9 . "<br>" . $conn->error;}	
 
 
 if ($conn->query($sqldel10) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND USNBT from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED USNBT from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel10 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel11) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND USD from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED USD from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}		
+	{echo "Error: " . $sqldel11 . "<br>" . $conn->error;}		
 
 
 if ($conn->query($sqldel12) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND BCH from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED BCH from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel12 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel13) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND CNNBT from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED CNNBT from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel13 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel14) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND DASH from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED DASH from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel14 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel15) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND LTC from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED LTC from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel15 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel16) === TRUE) 
-	{echo "SUCCESSFULLY CLEAND TUSD from $exchange <br />";} 
+	{echo "SUCCESSFULLY CLEANED TUSD from $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel16 . "<br>" . $conn->error;}
+
+
+if ($conn->query($sqldel18) === TRUE) 
+	{echo "SUCCESSFULLY CLEANED -NEO -ETH -KCS in $exchange <br />";} 
+else 
+	{echo "Error: " . $sqldel18 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel7) === TRUE) 
 	{echo "SUCCESSFULLY CHANGED NAMES in $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}
+	{echo "Error: " . $sqldel7 . "<br>" . $conn->error;}
 
 
 if ($conn->query($sqldel17) === TRUE) 
 	{echo "SUCCESSFULLY CHANGED SPECIALNAMES in $exchange <br />";} 
 else 
-	{echo "Error: " . $sqlwr . "<br>" . $conn->error;}	
+	{echo "Error: " . $sqldel17 . "<br>" . $conn->error;}	
 $conn->close();
---
+++ core/kucoin/kucoin-complete.php
@@ -0,0 +1,25 @@
+<?php
+// Name the exchange
+$ex = 'kucoin';
+// Where do I get my JSON information from?
+$url = "https://api.kucoin.com/v1/market/open/symbols";
+// Where do I get my BTC-JSON information from? 
+$url2 = "https://api.crex24.com/CryptoExchangeService/BotPublic/ReturnTicker?request=[NamePairs=USD_BTC]";
+// Decode the JSON
+$json = json_decode(file_get_contents($url), true);
+// Decode the JSON 2
+$json2 = json_decode(file_get_contents($url2), true);
+// Where do I get my USD-BTC value from? "0" is for a [ bracket (key)
+$price2 = $json2["Tickers"] ["0"] ["Last"];
+// Go through every key from "Tickers" and set PairName as $coinsname and Last as $price
+foreach ($json ["data"] as $key => $value) 
+	{
+		$price = $value["lastDealPrice"];
+		$coinsname = $value["symbol"];
+		$priceUSD = $price * $price2;
+		$ch = curl_init("http://127.0.0.1:8080/core/write-sql.php?ex=$ex&coin=$coinsname&price=$price&usd=$priceUSD");
+		curl_exec($ch);
+	}
+$ch2 = curl_init ("http://127.0.0.1:8080/core/clean-db.php?ex=$ex");
+curl_exec($ch2);
+?>
