474c6974e649f6816686d0723c2b07ed09234c30
Mike <31688573+zPools@users.noreply.github.com>
2018-10-08 16:33:10 +0200

    Update run-updates.sh

--- core/cronjob/run-updates.sh
+++ core/cronjob/run-updates.sh
@@ -4,6 +4,9 @@ php7.0 /var/www/html/API-reader/core/crex/crex-complete.php
 php7.0 /var/www/html/API-reader/core/poloniex/polo-complete.php
 php7.0 /var/www/html/API-reader/core/southxchange/southxchange-complete.php
 php7.0 /var/www/html/API-reader/core/kucoin/kucoin-complete.php
+php7.0 /var/www/html/API-reader/core/novaexchange/novaexchange-complete.php
+php7.0 /var/www/html/API-reader/core/stex/stex-complete.php
+php7.0 /var/www/html/API-reader/core/cryptopia/cryptopia-complete.php
 
 echo
 echo
